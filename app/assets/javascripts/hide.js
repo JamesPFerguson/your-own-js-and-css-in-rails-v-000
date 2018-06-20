8bbed46e57ead6bfaf36e905f01b7530c8a41df1
James Ferguson <ncjamesferguson@gmail.com>
2018-06-20 23:10:57 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,5 @@
-function hideWhenClicked(element) {
-  
-}
+const to_hide = document.getElementById('hide_this')
+
+to_hide.addEventListener('click', function hideWhenClicked(element) {
+  document.getElementById('hide_this').style.display = 'none';
+});
